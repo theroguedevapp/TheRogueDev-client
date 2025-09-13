237ab9a849881879611beb311e9ded86a6ca866b
Lucas Henrique <lucazsbs15@gmail.com>
2025-09-12 22:35:24 -0300

    Minor changes to the login/header, add functions to direct routes and register page create

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
