944a180a442c62d3b9287e78cc231caa5d0c387f
@devhenrilucas <lucazsbs15@gmail.com>
2025-10-16 02:44:12 -0300

    Login works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,20 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [{
     path: "login",
     component: LoginComponent,
+    canActivate: [authGuard]
 },
 {
     path: "",
-    component: HomeComponent,
+    component: HomeComponent
 },
 {
     path: "register",
     component: RegisterComponent,
-},
-{
-    path: "user",
-    component: UserProfileComponent,
+    canActivate: [authGuard]
 }
 ];
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { RegisterRequest, User } from '../../interfaces/user/user.interface';
-import { UserService } from '../user/user.service';
-import { Observable } from 'rxjs';
-import { BaseApiService } from '../base/base-api.service';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { LoginRequest, LoginResponse, RegisterRequest, User } from '../../interfaces/user/user.interface';
+import { BehaviorSubject, catchError, Observable, tap, of } from 'rxjs';
 import { ENVIRONMENT } from '../../enviroments/enviroment';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +12,77 @@ import { ENVIRONMENT } from '../../enviroments/enviroment';
 export class AuthService {
 
   protected readonly apiUrl = ENVIRONMENT.apiUrl;
+  private userSubject = new BehaviorSubject(<User | null>(null));
+  public user$ = this.userSubject.asObservable();
+  private _isInitialized = false;
+  private redirecUrl: string | null = null;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) { }
 
   register(registerData: RegisterRequest): Observable<User> {
     return this.http.post<User>(`${this.apiUrl}/v1/auth/register`, registerData);
   }
 
+  login(loginData: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/v1/auth/login`, loginData).pipe(
+      tap(response => {
+        this.userSubject.next(response.user);
+      })
+    );
+  }
+
+  logout(): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/v1/auth/logout`, {}).pipe(
+      tap(() => this.userSubject.next(null)));
+  }
+
+  loadUser(): Observable<User | null> {
+    if (!isPlatformBrowser(this.platformId)) {
+      return of(null);
+    }
+
+    return this.http.get<User>(`${this.apiUrl}/api/v1/user`).pipe(
+      tap((user) => {
+        this.userSubject.next(user);
+        this._isInitialized = true;
+      }),
+      catchError(() => {
+        this.userSubject.next(null);
+        this._isInitialized = true;
+        return of(null);
+      })
+    );
+  }
+
+  initAuth(): Observable<User | null> {
+    if (!isPlatformBrowser(this.platformId) || this._isInitialized) {
+      return of(this.userSubject.value);
+    }
+
+    return this.loadUser();
+  }
+
+  get isInitialized() {
+    return this._isInitialized;
+  }
+
+  get userLogged(): boolean {
+    return !!this.userSubject.value;
+  }
+
+  get currentUser(): User | null {
+    return this.userSubject.value;
+  }
+
+  setRedirectUrl(url: string): void{
+    this.redirecUrl = url;
+  }
+
+  getRedirectUrl(): string | null {
+    return this.redirecUrl;
+  }
+
+  clearRedirectUrl(): void {
+    this.redirecUrl = null;
+  }
 }
